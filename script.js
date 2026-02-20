e57c65133403ec67aa315686966ce76f66b8532b
Miaa82892024 <miaa82892024@gmail.com>
2026-02-19 23:48:15 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+function downloadPDF(){
+    
+    const element=document.querySelector('#pdf-content');
+    //console.log("element");
+
+    const otp={
+        margin: [10, 5, 15, 5], //[arriba, izquierda, abajo, derecha] en nm
+        filename: 'Hoja_de_vida_Isabel_Alzate_Arias.pdf',
+        image: { type: 'jpeg', quality:1},
+        html2canvas:{
+            scale: 2,
+            useCORS: true,
+            scrollY:0
+        },
+        jsPDF:{
+            unit:'mm',
+            format:'a4',
+            orientation: 'portrait' //Orientación vertical
+        }
+    }
+
+    html2pdf().set(otp).from(element).save();
+    
+    
+}
